5d09cae44ce12409f7fd1a2eef4fca629399daa9
KingNNT <Dev.KingNNT@gmail.com>
2021-04-07 10:51:22 +0700

    Update: Footer

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,3 +1,11 @@
-</body>
+	<footer class="position-fixed w-100" style="bottom: 0">
+		<div class="bg-light text-center text-lg-start">
+			<div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+				© <?= date('Y') ?> Copyright:
+				<a class="text-dark" href="#">HNUE.com</a>
+			</div>
+		</div>
+	</footer>
+	</body>
 
-</html>
+	</html>
